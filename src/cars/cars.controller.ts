b1a4adefe12f7cbe82f6e8afb44bc9fea4e6ba43
Yinner Chilito <yinnermedina30@gmail.com>
2024-02-21 11:42:29 -0500

    Finished section 'Primeros pasos en Nest'

---
+++ src/cars/cars.controller.ts
@@ -0,0 +1,48 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CarsService } from './cars.service';
+
+@Controller('cars')
+export class CarsController {
+  constructor(private readonly carsService: CarsService) {}
+
+  @Get()
+  getAllCars() {
+    return this.carsService.getAll();
+  }
+
+  @Get(':id')
+  getCarById(@Param('id', ParseIntPipe) id: number) {
+    console.log({ id });
+    // throw new Error('Auxilio...');
+    return this.carsService.findOneById(id);
+  }
+
+  @Post()
+  createCar(@Body() body: any) {
+    return {
+      body,
+    };
+  }
+  @Patch(':id')
+  updateCar(@Param('id', ParseIntPipe) id: number, @Body() body: any) {
+    return {
+      body,
+    };
+  }
+  @Delete(':id')
+  deleteCar(@Param('id', ParseIntPipe) id: number) {
+    return {
+      msg: 'Deleted',
+      id,
+    };
+  }
+}
